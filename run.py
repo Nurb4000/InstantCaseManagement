aa4659274df673306cfa6fdb611b3349a15f9de3
Nurb4000 <tony@instaview.com>
2026-06-21 17:05:33 -0400

    Use environment variables for Flask host and port

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    host = os.environ.get('FLASK_RUN_HOST', '0.0.0.0')
+    port = int(os.environ.get('FLASK_RUN_PORT', 5000))
+    app.run(host=host, port=port, debug=True)
